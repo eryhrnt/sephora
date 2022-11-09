c7e76c5ed9e50220a5c1dfa0a70379cda0b53133
Ery Harinanto <eryharinanto111@gmail.com>
2022-11-09 10:55:57 +0700

    change term

--- lib/register/screen/term_condition_screen.dart
+++ lib/register/screen/term_condition_screen.dart
@@ -16,9 +16,52 @@ class TermAndConditionScreen extends StatelessWidget {
                 children: [
                   RegisterAppBar(
                     onPressed: () {},
-                    title: '',
+                    title: 'Syarat & Ketentuan',
                   ),
-                  Expanded(child: Card())
+                  Expanded(
+                      child: Card(
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Image(
+                              image: AssetImage('assets/register/term.png'),
+                              width: 40,
+                              height: 40,
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.only(left: 10.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    const Text(
+                                      'Informasi Data Diri',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w600,
+                                        fontSize: 14,
+                                        color: Color(0xFF2B2F3C),
+                                      ),
+                                    ),
+                                    SizedBox(height: 2),
+                                    const Text(
+                                      'Masukkan informasi data diri Anda '
+                                      'untuk proses pembuatan akun.',
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 12,
+                                        color: Color(0xFF727FA3),
+                                      ),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  ))
                 ],
               ),
             ),
