b4ee2af15bda23f3b14bc838e3aecec3e5841cf5
Ery Harinanto <eryharinanto111@gmail.com>
2022-11-09 12:03:42 +0700

    add

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const TermAndConditionScreen(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(title: 'Flutter Demo Home Page'),
     );
   }
 }
